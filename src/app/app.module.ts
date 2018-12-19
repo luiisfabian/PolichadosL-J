a631262f9fa80557cc4bd8c3ee7fe91d05a5f3d7
Luis Fabian Muñoz <luiisfabian96@gmail.com>
2018-12-18 19:55:55 -0500

    INICIALIZACION DEL PROYECTO EN BITBUCKET

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { ServicesComponent } from './services/services.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { AboutComponent } from './about/about.component';
+import { TeamComponent } from './team/team.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    NavbarComponent,
+    HeaderComponent,
+    ServicesComponent,
+    PortafolioComponent,
+    AboutComponent,
+    TeamComponent,
+    ClientsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
